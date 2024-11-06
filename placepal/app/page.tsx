0c3d7fa5d7b5ce4aa582cf4d48e5248174d6da6a
Ayim William <odhisayim@gmail.com>
2024-11-06 20:24:20 +0300

    made changes to page

--- placepal/app/page.tsx
+++ placepal/app/page.tsx
@@ -1,12 +1,9 @@
-
+import Categories from "./components/Categories";
 
 export default function Home() {
   return (
-    <main className="">
-      PlacePal
-      <h2 className="text-airbnb">
-        PlacePal rocks
-      </h2>
+    <main className="max-w-[1500px] mx-auto px-6">
+      <Categories />
     </main>
   );
 }
