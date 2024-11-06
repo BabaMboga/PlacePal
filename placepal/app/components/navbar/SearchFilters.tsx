b021f5f150398b7b0dfc5537ec6dbb196137258f
Ayim William <odhisayim@gmail.com>
2024-11-06 19:49:05 +0300

    made changes to cursor

--- placepal/app/components/navbar/SearchFilters.tsx
+++ placepal/app/components/navbar/SearchFilters.tsx
@@ -4,21 +4,21 @@ import React from 'react'
 const SearchFilters = () => {
   return (
     <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
-        <div>
+        <div className='hidden lg:block'>
             <div className="flex flex-row items-center justify-between">
-                <div className="w-[250px] h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
+                <div className="cursor-pointer w-[250px] h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
                     <p className="text-xs font-semibold">Where</p>
                     <p className="text-sm ">Wanted Location</p>
                 </div>
-                <div className="h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
+                <div className="cursor-pointer h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
                     <p className="text-xs font-semibold">Check In</p>
                     <p className="text-sm ">Add dates</p>
                 </div>
-                <div className="h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
+                <div className="cursor-pointer h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
                     <p className="text-xs font-semibold">Check Out</p>
                     <p className="text-sm ">Add dates</p>
                 </div>
-                <div className="h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
+                <div className="cursor-pointer h-[64px] px-8 flex-col justify-center rounded-full hover:bg-gray-100">
                     <p className="text-xs font-semibold">Who</p>
                     <p className="text-sm ">Add guests</p>
                 </div>
