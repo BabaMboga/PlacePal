e00af10d2c51327410ccee8f14218104af70c114
Ayim William <odhisayim@gmail.com>
2024-11-06 20:11:52 +0300

    added Property button

---
+++ placepal/app/components/navbar/AddPropertyButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddPropertyButton = () => {
+  return (
+    <div className='p-2 text-xs lg:text-sm font-semibold rounded-full hover:bg-gray-200'>
+        PlacePal your home
+    </div>
+  )
+}
+
+export default AddPropertyButton
