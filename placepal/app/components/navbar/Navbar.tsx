0d0bf68bd1a87f33e111624783b35fb4b594aee2
Ayim William <odhisayim@gmail.com>
2024-11-06 19:44:24 +0300

    changed icon to svg

--- placepal/app/components/navbar/Navbar.tsx
+++ placepal/app/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import SearchFilters from "./SearchFilters";
+
 const Navbar = () => {
     return (
         <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
@@ -16,7 +18,7 @@ const Navbar = () => {
                     </Link>
 
                     <div className="flex space-x-6">
-                        Search Filters
+                        <SearchFilters />
                     </div>
 
                     <div className="flex items-center space-x-6">
