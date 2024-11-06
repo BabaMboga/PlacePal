84cfcf16dbe63bf129e1fcb9c84813b4946ccd72
Ayim William <odhisayim@gmail.com>
2024-11-06 20:46:54 +0300

    added beach category image

--- placepal/app/components/Categories.tsx
+++ placepal/app/components/Categories.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <div className='pt-3 cursor-pointer pb-6 flex items-center space-x-12'>
+        <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
+            <Image
+                src="/sun-umbrella.png"
+                alt="Category - Beach" 
+                width={20}
+                height={20}
+            />
+        </div>
+    </div>
   )
 }
 
