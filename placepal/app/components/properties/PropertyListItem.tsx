f0ee7b8aa1832bde4bbdb7a950000412847ff292
Ayim William <odhisayim@gmail.com>
2024-11-07 10:43:34 +0300

    detail page added

--- placepal/app/components/properties/PropertyListItem.tsx
+++ placepal/app/components/properties/PropertyListItem.tsx
@@ -4,11 +4,24 @@ import Image from 'next/image';
 const PropertyListItem = () => {
   return (
     <div className='cursor-pointer'>
-        <div className="overflow-hidden aspect-square rounded-xl">
+        <div className="relative overflow-hidden aspect-square rounded-xl">
             <Image
                 fill
-                src="/placepal-images/"
+                src="/placepal-images/Beach-1.jpeg"
+                alt="beach home"
+                sizes="{max-width: 768px} 768px, {max-width: 1200px}: 768px, 768px"
+                className='hover:scale-110 object-cover tranisition h-full w-full'
             />
+        </div>
+
+        <div className='mt-2'>
+            <p className="text-lg font-bold"></p>
+        </div>
+
+        <div className='mt-2'>
+            <p className='text-sm text-gray-500'>
+                <strong>$200</strong> per night
+            </p>
         </div>
     </div>
   )
